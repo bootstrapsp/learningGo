6f05c71a18662b3e06860b616a132178b6f1bb17
WANDERERSBOOK\pandey <sushant.pandey@wanderersbook.com>
2018-01-02 23:00:01 +0100

    Added comments in the blank identifier

--- learningGo/BlankIdentifier/main.go
+++ learningGo/BlankIdentifier/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	// "_" is the blank identifier which tells Go to dump the error
+	// it tells the compiler something in the code is not being used.
 	res, _ := http.Get("https://wanderersbook.com")
 	page, _ := ioutil.ReadAll(res.Body)
 	res.Body.Close()
